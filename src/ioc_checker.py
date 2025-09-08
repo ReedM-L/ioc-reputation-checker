27a77b5b9194e83b9fa07752befaf35da2870847
Reed Lyle <lylereedm@gmail.com>
2025-09-08 13:56:33 -0400

    Did error and logic fixes, added more to api check of provided IP, have not yet gotten to test it, need to make sure I formatted the API call correctly. Also need to format the response to a json file after.

--- src/ioc_checker.py
+++ src/ioc_checker.py
@@ -1,5 +1,7 @@
 import ipaddress
 import requests
+import os
+import json
 #!/usr/bin/env python3
 
 """
@@ -7,30 +9,44 @@ IOC Checker Tool
 ----------------
 Takes an IP by user input or a list by file and checks them against threatintelligence sources
 """
-def check_ip_abuseipdb(ip: str, api_key: str):
-    
 
+def check_ip_abuseipdb(ip: str):
+    url = 'https://api.abuseipdb.com/api/v2/check'
+
+    query_string = {
+        'ipAddress': ip,
+        'maxAgeInDays': '90'
+    }
+
+    headers = {
+        'Accept': 'application/json',
+        'Key': os.environment.get("ABUSEIPDB_API_KEY")
+    }
+    
+    response = requests.request(method='GET', url=url, headers=headers, params=querystring)
 
 def validate_addr(user_ip):
     #use ipaddress() to check if the user input is a valid address, catch the ValueError if not
-    valid_status = false
+    valid_status = False
 
     try: 
-        user_ip = ipaddress.ipaddress(user_ip)
+        user_ip = ipaddress.ip_address(user_ip)
         print("Valid IP entered, proceeding...")
-        valid_status = true
+        valid_status = True
         return valid_status
     except:
         print("Invalid IP address entered")
         return valid_status
 
 def main():
-    user_ip = input("Enter the IP to check")
+    valid_status = False
 
-    While valid_status != true:
-        valid_status = validate_addr(user_ip) 
-
+    while valid_status != True:
+        user_ip = input("Enter the IP to check: ")  
+        valid_status = validate_addr(user_ip)
     
+    
+
 
 if __name__ == "__main__":
     main()

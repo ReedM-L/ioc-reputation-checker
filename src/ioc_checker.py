d9a6c6e1dfeb5c06487998e7c9e33a901598231b
Reed Lyle <lylereedm@gmail.com>
2025-09-30 16:13:33 -0400

    Imported argparse library, added two flags, --ip --file, depending which the program is called with the program will either ask for a singular ip to be checked, or if a valid file name is passed it will be parsed and have the ips stripped out and checked one by one, need to work on formatting output better potentially creating a file/table of the output. Also need to fix some argparse issues, if you call --ip the program doesn't use the one it was called with and instead asks for input. Overarching goal is to implement 1 - 2 more APIs to get a score that queries more sources, and deal with better output formatting and logic.

--- src/ioc_checker.py
+++ src/ioc_checker.py
@@ -1,7 +1,7 @@
 import ipaddress
 import requests
 import os
-import json
+import argparse
 #!/usr/bin/env python3
 
 """
@@ -10,7 +10,7 @@ IOC Checker Tool
 Takes an IP by user input or a list by file and checks them against threatintelligence sources
 """
 
-def check_ip_abuseipdb(ip: str):
+def check_ip_abuseipdb(ip: str) -> dict:
     url = 'https://api.abuseipdb.com/api/v2/check'
 
     query_string = {
@@ -25,31 +25,57 @@ def check_ip_abuseipdb(ip: str):
     
     response = requests.request(method='GET', url=url, headers=headers, params=query_string)
 
-    data = response.json()
+    response.raise_for_status()
 
-    print(data)
+    data = response.json()["data"]
 
-def validate_addr(user_ip: str):
+    #return a dict containing just IP and confidence for now
+    return {
+        "ip": data.get("ipAddress"),
+        "confidence": data.get("abuseConfidenceScore"),
+    }
+
+def validate_addr(user_ip: str) -> bool:
     #use ipaddress() to check if the user input is a valid address, catch the ValueError if not
-    valid_status = False
-
     try: 
-        user_ip = ipaddress.ip_address(user_ip)
+        ipaddress.ip_address(user_ip)
         print("Valid IP entered, proceeding...")
-        valid_status = True
-        return valid_status
-    except:
+        return True
+    except ValueError:
         print("Invalid IP address entered")
-        return valid_status
+        return False
 
 def main():
+    parser = argparse.ArgumentParser(
+                        prog="ioc_reputation_checker",
+                        description="Takes in either a single IP to check or a lists of IPs from a file")
+
+    #arguments to specify if user is inputting singular input or a file of inputs
+    parser.add_argument("--ip", help = "Check a single IP")
+    parser.add_argument("--file", help = "Path to input file of IPs to check")
+    args = parser.parse_args()
+    
     valid_status = False
+    print("Confidence Score is a 0 - 100 scale, 0 meaning safe, 100 meaning confidently malicious")
+    if args.ip:
+        while not valid_status:
+            input_ip = input("Enter the IP to check: ")
+            valid_status = validate_addr(input_ip)
+        data = check_ip_abuseipdb(input_ip)
+        print(f"IP: {input_ip} -> Confidence Score (0 - 100): {data['confidence']}")
 
-    while valid_status != True:
-        user_ip = input("Enter the IP to check: ")  
-        valid_status = validate_addr(user_ip)
 
-    check_ip_abuseipdb(user_ip)
+    if args.file:
+        with open(args.file, "r") as f:
+            for address in f:
+                address = address.strip()
+                valid_status = validate_addr(address)
+                if (valid_status):
+                    #TODO:Add more functionality, want to create an output file maybe of each ip with a corresponding report
+                    data = check_ip_abuseipdb(address)
+                    print(f"IP: {address} -> Confidence Score (0 - 100): {data['confidence']}")
+                else:
+                    print("Ill formatted IP: " + address)
 
 if __name__ == "__main__":
     main()
